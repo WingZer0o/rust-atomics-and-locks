6cb84caca0c04300b58a60e8a7d14b0a7d536b13
Mike Mulchrone <mtmulch0191@outlook.com>
2023-03-18 22:12:06 -0400

    two_threads, closure_thread, and returning values from thread

---
+++ chapter1/src/main.rs
@@ -0,0 +1,44 @@
+use std::thread;
+
+fn main() {
+    // two_threads();
+    // closure_thread();
+    // closure_thread_get_value_back();
+}
+
+fn closure_thread_get_value_back() {
+    let numbers = Vec::from_iter(0..=1000);
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.iter().sum::<usize>();
+        return (sum / len);
+    });
+
+    let average = t.join().unwrap();
+    println!("The average is: {:?}", average);
+}
+
+fn closure_thread() {
+    let numbers = vec![1, 2, 3];
+    thread::spawn(move || {
+        for n in &numbers {
+            println!("{n}");
+        }
+    }).join().unwrap();
+}
+
+fn two_threads() {
+    let t1 = thread::spawn(f);
+    let t2 = thread::spawn(f);
+
+    println!("Hello from the main thread!");
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+}
+
+fn f() {
+    println!("Hello from a thread!");
+    let id = thread::current().id();
+    println!("This is my thread id: {:?}", id);
+}
